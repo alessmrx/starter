4f6fd628898f53a232b1abd81fb072d9326709bf
alessmrx <alesstest.64@gmail.com>
2025-06-20 18:24:00 +0200

    add code base

---
+++ index.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# memoire 
+local number=0;
+
+# programme
+while [ $number -lt 5 ]; do # boucle infinie
+    echo -n "voulez-vous continuer ? (o/n): "
+    read reponse
+    if [[ $reponse = 'n' ]];then
+        break;
+    fi
+    number=$((number + 1)); # incrementation de la variable number
+    echo $number; # affichage de la variable number
+done # fin de la boucle
+
+echo "fin de programme"
